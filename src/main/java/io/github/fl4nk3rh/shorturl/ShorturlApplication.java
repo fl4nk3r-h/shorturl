1bd3266515d0bfbb525a9958548a37b438fc7b52
fl4nk3r-h <fl4nk3r.7h3.b4ckb0n3@gmail.com>
2026-09-22 22:08:51 +0530

    Initialize Spring Boot project with Maven wrapper and basic application structure

---
+++ src/main/java/io/github/fl4nk3rh/shorturl/ShorturlApplication.java
@@ -0,0 +1,13 @@
+package io.github.fl4nk3rh.shorturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorturlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShorturlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/fl4nk3rh/shorturl/ShorturlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fl4nk3rh.shorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorturlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
